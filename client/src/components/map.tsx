30a43c2bc92914fb3ce8ad70e4785c5d9b520cf0
Haakon Gunnarsli <haakgu@stud.ntnu.no>
2021-09-23 21:56:25 +0200

    map fix

--- client/src/components/map.tsx
+++ client/src/components/map.tsx
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import locations from "../data/locations.json";
+import React, { useState } from 'react';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import locations from '../data/locations.json';
 
-const fs = require("fs");
+const fs = require('fs');
 
 export type VoyageLegs = {
-  VesselName: String;
-  PORT_UN_FROM: String;
-  PORT_UN_TO: String;
-  HOURS_UNDERWAY: number;
-  total_distance: number;
-  CARGO_to_DWT_pct: number;
-  Total_CO2: number;
-  StandardShipDesign: String;
-  VesselTypeGroup: String;
+    VesselName: String;
+    PORT_UN_FROM: String;
+    PORT_UN_TO: String;
+    HOURS_UNDERWAY: number;
+    total_distance: number;
+    CARGO_to_DWT_pct: number;
+    Total_CO2: number;
+    StandardShipDesign: String;
+    VesselTypeGroup: String;
 };
 
 export type AISPOSITION = {
-  VesselName: String;
-  From: String;
-  To: String;
-  LoadingCondition: String;
-  hoursSinceLastPosition: number;
-  longitude: number;
-  latitude: number;
+    VesselName: String;
+    From: String;
+    To: String;
+    LoadingCondition: String;
+    hoursSinceLastPosition: number;
+    longitude: number;
+    latitude: number;
 };
 
 const Map = () => {
-  /*
+    /*
   fs.readFile(
     "../../data/locations.json",
     "utf8",
@@ -42,26 +42,28 @@ const Map = () => {
   );
 */
 
-  console.log(locations);
-  return (
-    <MapContainer
-      center={[59.90688962048543, 10.76030652299994]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    console.log(locations);
+    const foo = locations.filter((loc, i) => i < 100);
+    console.log(foo);
+    return (
+        <MapContainer
+            center={[parseFloat(foo[0].latitude), parseFloat(foo[0].longitude)]}
+            zoom={13}
+            scrollWheelZoom={false}
+        >
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-      {locations.map(({ latitude, longitude }) => (
-        <Marker
-          key={`${latitude}${longitude}`}
-          position={[parseFloat(latitude), parseFloat(longitude)]}
-        ></Marker>
-      ))}
-    </MapContainer>
-  );
+            {foo.map((loc, i) => (
+                <Marker
+                    key={`${loc.latitude}${loc.longitude}`}
+                    position={[parseFloat(loc.latitude), parseFloat(loc.longitude)]}
+                ></Marker>
+            ))}
+        </MapContainer>
+    );
 };
 
 export default Map;
