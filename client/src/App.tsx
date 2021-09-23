d9a450e5689dcc07de1156365287fa73e4486276
Haakon Gunnarsli <haakgu@stud.ntnu.no>
2021-09-23 21:44:30 +0200

    adds map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Map from "./components/Map";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/map.tsx
+++ client/src/components/map.tsx
@@ -1,1 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import locations from "../data/locations.json";
+
+const fs = require("fs");
+
+export type VoyageLegs = {
+  VesselName: String;
+  PORT_UN_FROM: String;
+  PORT_UN_TO: String;
+  HOURS_UNDERWAY: number;
+  total_distance: number;
+  CARGO_to_DWT_pct: number;
+  Total_CO2: number;
+  StandardShipDesign: String;
+  VesselTypeGroup: String;
+};
+
+export type AISPOSITION = {
+  VesselName: String;
+  From: String;
+  To: String;
+  LoadingCondition: String;
+  hoursSinceLastPosition: number;
+  longitude: number;
+  latitude: number;
+};
+
+const Map = () => {
+  /*
+  fs.readFile(
+    "../../data/locations.json",
+    "utf8",
+    (err: any, jsonString: any) => {
+      if (err) {
+        console.log("File read failed:", err);
+        return;
+      }
+      console.log("File data:", jsonString);
+      setLocation(JSON.parse(jsonString));
+    }
+  );
+*/
+
+  console.log(locations);
+  return (
+    <MapContainer
+      center={[59.90688962048543, 10.76030652299994]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {locations.map(({ latitude, longitude }) => (
+        <Marker
+          key={`${latitude}${longitude}`}
+          position={[parseFloat(latitude), parseFloat(longitude)]}
+        ></Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default Map;
