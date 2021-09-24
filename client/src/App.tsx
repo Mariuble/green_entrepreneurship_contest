caf4f0f69a70bc6080a75d365198cb0834b99a59
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-09-24 09:51:52 +0200

    landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,21 @@ import { useReducer } from 'react';
 import './App.css';
 import Map from './components/LeafletMap';
 import VesselContainer from './components/VesselContainer';
+import Landing from './components/Landing';
+import { ChakraProvider } from '@chakra-ui/react';
 import { initialState, ShipContext, shipReducer } from './context/ShipContext';
 
 function App() {
     const [state, dispatch] = useReducer(shipReducer, initialState);
     return (
         <div className="App">
-            <ShipContext.Provider value={{ state, dispatch }}>
-                {/* <Map /> */}
-                <VesselContainer />
-            </ShipContext.Provider>
+            <ChakraProvider>
+                <ShipContext.Provider value={{ state, dispatch }}>
+                    <Landing />
+                    {/* <Map /> */}
+                    <VesselContainer />
+                </ShipContext.Provider>
+            </ChakraProvider>
         </div>
     );
 }
