4db3359c88906bce1ec6bddf80ecc260d97a5193
mariuble <mariuble@stud.ntnu.no>
2021-09-24 10:18:27 +0200

    Chakra theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,19 @@ import { useReducer } from 'react';
 import './App.css';
 import Map from './components/LeafletMap';
 import VesselContainer from './components/VesselContainer';
+import { ChakraProvider } from '@chakra-ui/provider';
 import { initialState, ShipContext, shipReducer } from './context/ShipContext';
 
 function App() {
     const [state, dispatch] = useReducer(shipReducer, initialState);
     return (
         <div className="App">
-            <ShipContext.Provider value={{ state, dispatch }}>
-                {/* <Map /> */}
-                <VesselContainer />
-            </ShipContext.Provider>
+            <ChakraProvider>
+                <ShipContext.Provider value={{ state, dispatch }}>
+                    {/* <Map /> */}
+                    <VesselContainer />
+                </ShipContext.Provider>
+            </ChakraProvider>
         </div>
     );
 }
